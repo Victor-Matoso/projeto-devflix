b651106c70910a68102ee63bebc4959dec4ebbd7
Victor-Matoso <victorrodriguesmjau@gmail.com>
2023-08-31 17:09:18 -0300

    projeto quase finalizado

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,72 +1,114 @@
 import { useEffect, useState } from "react";
 
-import logo from "../assets/devflix.png"
-import searchIcon from "../assets/search.svg"
-import "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
+import logo from "../assets/devflix2.png";
+import searchIcon from "../assets/Vector.svg";
+import "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js";
 
 import "./App.css";
 import MovieCard from "../components/MovieCard/movieCard";
 import Footer from "../components/footer/footer";
 
 const App = () => {
-const [searchTerm,setSearchTerm]= useState("");
-const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
 
-const apiKey = "eee79e61";
-    const apiUrl =`https://omdbapi.com/?apikey=${apiKey}`;
+  const apiKey = "eee79e61";
+  const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`;
 
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
 
-    useEffect(() => {
-        searchMovies("Batman");
-    }, []);
+  const searchMovies = async (title) => {
+    const response = await fetch(`${apiUrl}&s=${title}`);
+    const data = await response.json();
 
-    const searchMovies = async (title) => {
-        const response = await fetch(`${apiUrl}&s=${title}`)
-        const data = await response.json();
+    console.log(data);
+    setMovies(data.Search);
+  };
 
-        console.log(data);
-        setMovies(data.Search);
-    };
+  // JEITO MAIS COMPLEXO DE FAZER A API
 
-    
+  // fetch(apiUrl)
+  // .then((response) => response.json())
+  // .then((data) => console.log(data));
 
-// JEITO MAIS COMPLEXO DE FAZER A API 
-
-// fetch(apiUrl)
-// .then((response) => response.json())
-// .then((data) => console.log(data));
-    
-const handleKeyPress = (e) => {
-  e.key ==="Enter" && searchMovies(searchTerm)
-};
+  const handleKeyPress = (e) => {
+    e.key === "Enter" && searchMovies(searchTerm);
+  };
 
   return (
-    <div id="app">
-      <div className="logo">
-        <img src={logo} alt="logo devflix" />
+    <>
+    <div className="divMaster">
+      <div id="app">
+        {/* <div className="cabecalhoMaster"> */}
+        <div className="logo">
+          <img src={logo} alt="logo devflix" />
+        </div>
+        <div className="navegacao">
+          <ul>
+            <li>
+              <a href="#">Início</a>
+            </li>
+            <li>
+              <a href="#">Filmes</a>
+            </li>
+            <li>
+              <a href="#">Séries</a>
+            </li>
+          </ul>
+        </div>
+        <div className="search">
+          <input
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            onKeyDown={handleKeyPress}
+            placeholder="Pesquise por filmes"
+          />
+          <img
+            src={searchIcon}
+            alt="Icone de pesquisa"
+            onClick={() => searchMovies(searchTerm)}
+          />
+        </div>
+        {/* </div> */}
       </div>
-      <div className="search">
-        <input 
-        value={searchTerm} 
-        onChange={(e) => setSearchTerm(e.target.value)}
-        onKeyDown={handleKeyPress}
-        placeholder="Pesquise por filmes"
-        />
-        <img src={searchIcon} 
-        alt="Icone de pesquisa"
-        onClick={() => searchMovies(searchTerm)}
-        />
+      <div className="espacotitle">
+        <h2>
+          Em alta:
+        </h2>
       </div>
-      {movies?.length > 0 ? (<div className="container">
-        {movies.map((movie) => (<MovieCard key={movie.imdbID} movies={movie} />))}
-    </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          <section className="sect">
+          {movies.map((movie) => (
+            <MovieCard key={movie.imdbID} movies={movie} />
+          ))}
+          </section>
+          
+<h2>
+  Lançamentos
+</h2>
+          <section className="sect">
+          {movies.map((movie) => (
+            <MovieCard key={movie.imdbID} movies={movie} />
+          ))}
+          </section>
+          
+        </div>
+
+        
+
+        
+        
       ) : (
         <div className="empty">
           <h2>Nenhum filme encontrado 😢</h2>
         </div>
       )}
       <Footer link={"https:github.com.br"}>VictorMatoso</Footer>
-      </div>
+    </div>
+    </>
   );
 };
 
